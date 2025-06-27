912c1646b263f81ac878d35c10baf434bb846877
Gbohunmi Williams <elitegbohunmi@gmail.com>
2025-06-27 19:28:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
+
+const mobileSplit = new SplitText("#headline", { type: "chars" });
+
+gsap.from(mobileSplit.chars, {
+  scrollTrigger: {
+    trigger: "#headline",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  },
+  opacity: 0,
+  y: 20,
+  duration: 0.6,
+  ease: "power1.out",
+  stagger: 0.01,
+  delay: 2.5
+});
+
+ScrollReveal().reveal(".fir", {
+  distance: "50px",
+
+  delay: 2400,
+
+  origin: "bottom",
+
+  duration: 1000,
+
+  easing: "ease-in-out",
+});
+ScrollReveal().reveal(".for", {
+  distance: "50px",
+
+  delay: 2000,
+
+  origin: "bottom",
+
+  duration: 1000,
+
+  easing: "ease-in-out",
+});
+
+
+let menu = document.querySelector(".menu");
+function opens() {
+  document.querySelector("ul").classList.toggle("active");
+  document.querySelector(".menu").classList.toggle("yh");
+}
